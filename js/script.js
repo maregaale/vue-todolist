ea97f182e9b91154b29e30bdef3696d12fc7acb3
Alessandro Marega <ale_marega@hotmail.it>
2021-03-26 15:48:45 +0100

    aggiunta con Vue la lista

--- js/script.js
+++ js/script.js
@@ -2,7 +2,14 @@ const app = new Vue (
   {
     el: "#root",
     data: {
-
+      lista: [
+        "html",
+        "css",
+        "bootstrap",
+        "js",
+        "jquery",
+        "vuejs"
+      ],
     },
     methods: {
 
